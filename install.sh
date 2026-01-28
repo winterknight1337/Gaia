f2648979990d5315f7229bcec349442eae6a80bd
Shad Brown <winterknight@winterknight.net>
2026-01-28 00:21:55 -0600

    Add webhooks, registry browser, and ldap browser

--- install.sh
+++ install.sh
@@ -65,6 +65,15 @@ $PWD/mythic-cli install github https://github.com/MythicC2Profiles/smb.git;
 echo "Installing Forge!";
 sudo $PWD/mythic-cli install github https://github.com/MythicAgents/forge.git;
 
+echo "Installing Webhooks!";
+sudo $PWD/mythic-cli install github https://github.com/MythicC2Profiles/basic_webhook.git
+
+echo "Installing Registry Browser!"
+sudo $PWD/mythic-cli install github https://github.com/MythicC2Profiles/registry_browser.git
+
+echo "Installing LDAP Browser!"
+sudo $PWD/mythic-cli install github https://github.com/MythicC2Profiles/ldap_browser.git
+
 echo "Mythic webserver hosted and ready via HTTPS on port 7443!";
 admin_passwd=$(grep "MYTHIC_ADMIN_PASSWORD" /opt/Mythic/.env | cut -d '"' -f 2)
 echo "Use mythic_admin:$admin_passwd to connect to the C2 server!";
