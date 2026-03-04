4a6990fbb7829b61ae1db09d9ac40ba9012f41be
winterknight <winterknight@winterknight.net>
2026-03-03 21:26:44 -0600

    prepare for future auth submodules

--- gaia.py
+++ gaia.py
@@ -5,6 +5,7 @@ import utils.auth
 global_parser = argparse.ArgumentParser(
     prog='gaia.py',
     description='Lightweight helper tool to deploy and manage mythic c2 installs.')
+# Going to be using this janky method until 
 global_parser.add_argument('-s', '--mythic-server', required=True, nargs=1, metavar='', help="fqdn or ip address for mythic server")
 global_parser.add_argument('-p', '--mythic-port', required=True, nargs=1, metavar='', help="port for admin interface on mythic server. Defaults to 7443.")
 global_parser.add_argument('-aP', '--auth-password', nargs=1, metavar='', help='password for mythic user account')
@@ -13,18 +14,19 @@ global_parser.add_argument('-k', '--no-ssl', action='store_true', help='disable 
 
 # 'Core' modules
 subparsers = global_parser.add_subparsers(title='modules', help='')
+# auth_parser = subparsers.add_parser('auth', help='authenticate to mythic')
 # c2_profile_parser = subparsers.add_parser('c2-profiles', help='manage mythic c2 profiles')
 # dns_parser = subparsers.add_parser('dns', help='manage dns records')
 # install_parser = subparsers.add_parser('install', help='manage mythic installation')
 # operation_parser = subparsers.add_parser('operation', help='manage mythic operations')
 # payload_parser = subparsers.add_parser('payloads', help='manage payloads')
-# user_parser = subparsers.add_parser('users', help='manage mythic users')
+user_parser = subparsers.add_parser('users', help='manage mythic users')
 
 # user modules
-# action_user_parser = user_parser.add_mutually_exclusive_group(required=True)
-# action_user_parser.add_argument('-a', '--add', action='store_true', help='creates user accounts in mythic')
-# action_user_parser.add_argument('-d', '--delete', action='store_true', help='deletes user accounts in mythic')
-# user_parser.add_argument('-u', '--users', nargs='+', help='provide one or more user account to process')
+action_user_parser = user_parser.add_mutually_exclusive_group(required=True)
+action_user_parser.add_argument('-c', '--create', action='store_true', help='creates user accounts in mythic')
+action_user_parser.add_argument('-d', '--delete', action='store_true', help='deletes user accounts in mythic')
+user_parser.add_argument('-u', '--users', nargs='+', metavar='', help='provide one or more user account to process')
 
 # TODO implement the rest of these after getting basic POC out
 # user_parser.add_argument('-oF', '--output-file', metavar='path/to/output', help='dumps newly created credentials to disk')
@@ -35,6 +37,7 @@ args = global_parser.parse_args()
 
 async def main():
     global_parser.print_help()
+    user_parser.print_help()
 
     # Authenticates to mythic if server, port, user, and password are specified
     if args.auth_user != None and args.auth_password != None and args.mythic_server != None and args.mythic_port != None:
