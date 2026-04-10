e075f835c4e808b033342e0d44d8477116e0ecc9
winterknight <winterknight@winterknight.net>
2026-04-09 22:32:54 -0500

    Add operation retrival

--- utils/operations.py
+++ utils/operations.py
@@ -10,5 +10,8 @@ async def create_operation(mythic_instance: mythic, operation_name: str):
 
 async def add_operator_to_operation(mythic_instance: mythic, operation_name: str, username: str):
     results = await mythic.add_operator_to_operation(mythic=mythic_instance, operation_name=operation_name, operator_username=username)
-    print(results)
     return results
+
+async def get_operations(mythic_instance:mythic):
+    operations = await mythic.get_operations(mythic=mythic_instance)
+    return operations
