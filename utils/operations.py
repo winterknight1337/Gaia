c9d6a7c8a1ad61d2381c51df9f40e0583d231794
winterknight <winterknight@winterknight.net>
2026-04-09 22:39:36 -0500

    rearrange things to make more sense

--- utils/operations.py
+++ utils/operations.py
@@ -3,6 +3,10 @@
 from mythic import mythic
 from utils.auth import *
 
+async def get_operations(mythic_instance:mythic):
+    operations = await mythic.get_operations(mythic=mythic_instance)
+    return operations
+
 async def create_operation(mythic_instance: mythic, operation_name: str):
     results = await mythic.create_operation(mythic=mythic_instance, operation_name=operation_name)
     print(results)
@@ -11,7 +15,3 @@ async def create_operation(mythic_instance: mythic, operation_name: str):
 async def add_operator_to_operation(mythic_instance: mythic, operation_name: str, username: str):
     results = await mythic.add_operator_to_operation(mythic=mythic_instance, operation_name=operation_name, operator_username=username)
     return results
-
-async def get_operations(mythic_instance:mythic):
-    operations = await mythic.get_operations(mythic=mythic_instance)
-    return operations
