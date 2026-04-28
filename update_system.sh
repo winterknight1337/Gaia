26be654afd1624cf4db0f4c398e0eeef0cd770dd
winterknight <winterknight@winterknight.net>
2026-04-28 15:29:47 -0500

    cut down on flashiness of update message

--- update_system.sh
+++ update_system.sh
@@ -4,7 +4,6 @@ if [ "$EUID" -ne 0 ]
   then echo "[-] Please run as root";
   exit;
 fi
-echo "****Preparing to install Mythic Prerequisites!****"
 
 echo "Performing apt update and upgrade!"
 apt update && apt upgrade -y;
