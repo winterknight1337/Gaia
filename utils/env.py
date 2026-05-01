3e6fb927ff2b880fc3fb653be97d9a3bcbce3896
winterknight <winterknight@winterknight.net>
2026-05-01 15:30:02 -0500

    add back type hint for str

--- utils/env.py
+++ utils/env.py
@@ -1,4 +1,4 @@
-def update_env(env_key: str, env_value):
+def update_env(env_key: str, env_value: str):
     import dotenv, os, shutil
     if os.path.isfile(".env-template") == True and os.path.isfile(".env") == False:
         shutil.copy(".env-template", ".env")
