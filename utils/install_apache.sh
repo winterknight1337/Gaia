2d00c8eff1dec5cc657acaceb3185933ff367aaf
winterknight <winterknight@winterknight.net>
2026-07-16 21:11:44 -0500

    pre-install certbot

--- utils/install_apache.sh
+++ utils/install_apache.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+# Install apache
 apt install apache2 -y;
 a2enmod rewrite proxy proxy_http;
 systemctl restart apache2;
@@ -9,6 +10,12 @@ echo -e '\t Options Indexes FollowSymLinks MultiViews' >> /etc/apache2/sites-ena
 echo -e '\t AllowOverride All' >> /etc/apache2/sites-enabled/000-default.conf;
 echo -e '\t Require all granted' >> /etc/apache2/sites-enabled/000-default.conf;
 echo '</Directory>' >> /etc/apache2/sites-enabled/000-default.conf;
-
 touch /var/www/html/.htaccess;
-echo -e 'RewriteEngine on';
+systemctl restart apache2;
+
+# Certbot install
+apt install python3 python3-dev python3-venv libaugeas-dev gcc -y;
+python3 -m venv /opt/certbot
+/opt/certbot/bin/pip install --upgrade pip
+/opt/certbot/bin/pip install certbot certbot-apache
+ln -s /opt/certbot/bin/certbot /usr/local/bin/certbot
